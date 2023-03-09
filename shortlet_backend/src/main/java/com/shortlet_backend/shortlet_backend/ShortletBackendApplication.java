6705f924b022a05b9dd0b913564fc49715e485a7
Bassey <inameti@systemspecs.com.ng>
2023-03-09 11:35:49 +0100

    creating the shortlet_backend

---
+++ shortlet_backend/src/main/java/com/shortlet_backend/shortlet_backend/ShortletBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shortlet_backend.shortlet_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortletBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortletBackendApplication.class, args);
+	}
+
+}
---
+++ shortlet_backend/src/test/java/com/shortlet_backend/shortlet_backend/ShortletBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortlet_backend.shortlet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortletBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
