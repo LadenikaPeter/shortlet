ec8e7bf0e02acfc32cd3b9f38b9ab98b20d3c35d
Ladenika <ladenika@systemspecs.com.ng>
2023-03-12 21:26:17 +0100

    authentication added

--- shortlet/src/app/app.module.ts
+++ shortlet/src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    HeaderComponent
-  ],
+  declarations: [AppComponent, HomeComponent, HeaderComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ shortlet/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'shortlet-app-4bae5',
+    appId: '1:9693322219:web:a9d1281ed00ad4784357ae',
+    storageBucket: 'shortlet-app-4bae5.appspot.com',
+    apiKey: 'AIzaSyCzmyhkTgM7nlz_VGHz7TbSZnPMA87YkwI',
+    authDomain: 'shortlet-app-4bae5.firebaseapp.com',
+    messagingSenderId: '9693322219',
+    measurementId: 'G-WKYRJF3ZP4',
+  },
+  production: false,
+};
