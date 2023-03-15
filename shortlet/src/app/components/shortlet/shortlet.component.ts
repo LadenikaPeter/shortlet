496292881848f641b6c6ccfbd114b67e0c1a294a
Ikwemogena Abdulai <abdulai@systemspecs.com.ng>
2023-03-15 15:02:50 +0100

    calendar - bill

--- shortlet/src/app/components/shortlet/shortlet.component.ts
+++ shortlet/src/app/components/shortlet/shortlet.component.ts
@@ -2,15 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataStorageService } from 'src/app/services/data-storage.service';
 
-import { differenceInDays} from 'date-fns';
-// import {  } from '@date-fns';
-
 @Component({
   selector: 'app-shortlet',
   templateUrl: './shortlet.component.html',
   styleUrls: ['./shortlet.component.css'],
 })
 export class ShortletComponent implements OnInit {
+  checkinDate: Date 
+  checkoutDate: Date
+  numberOfNights: number
   shortletData: any= []
   shortletPictures: any = [];
   showAmenities: boolean = false;
@@ -25,23 +25,27 @@ export class ShortletComponent implements OnInit {
   ngOnInit() {
     this.activatedRoute.params.subscribe((data) => {
       let id: number = data['id'];
-
-      console.log(id);
       this.displayShortlet(id);
     });
 
+
     this.checkinDate = new Date()
-    console.log(this.checkinDate.getDate())
     
+    let newCheckoutDate = this.checkoutDate = new Date()
+    newCheckoutDate.setDate(new Date().getDate() + 2) // 2 days to the default checkin and out date
+
+    this.fetchDateSelected();
   }
 
+  
+
+  //to diplay hortlet
   displayShortlet(id: number) {
     this.dataStorage.displayShortlet(id).subscribe(
       (response) => {
-        // console.log(this.shortletData = response)
-        this.shortletData = response;
-        this.shortletPictures = response.pictures;
-
+        console.log(this.shortletData = response)
+        this.shortletData = response; //details of shortlet from API
+        this.shortletPictures = response.pictures; //pictures of shortlet from API
       },
       (error) => console.log(error)
     );
@@ -50,28 +54,31 @@ export class ShortletComponent implements OnInit {
   toggleAmenities(){
     this.showAmenities != this.showAmenities; 
   }
-
-  // dateSelected: any
-  checkinDate: Date 
-  checkoutDate: Date
-
-  // dateLeft: number
-  // dateRight: number
-
   fetchDateSelected() {
-    console.log("the check in date: " + this.checkinDate);
-    console.log("the check out date: " + this.checkoutDate);
-    console.log()
-    // console.log()
-    this.showNight();
+    
+    
+    let timeDiff = Math.abs(new Date(this.checkoutDate).getTime() - new Date(this.checkinDate).getTime())
+    this.numberOfNights = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+    console.log(this.numberOfNights)
+
+    this.bill();
   }
 
-  showNight(){
-    let timeDiff = Math.abs(new Date(this.checkoutDate).getTime() - new Date(this.checkinDate).getTime())
-    // let timeDiff = Math.abs(1200);
-    let numberOfNights = Math.ceil(timeDiff / (1000 * 3600 * 24));
-    console.log(numberOfNights + " nights");
+  //reservation bill
+  calculateNumberOfNights: number
+  total: number
+
+  bill() {
+    console.log(this.numberOfNights)
+    console.log(this.calculateNumberOfNights)
+
+
+    console.log(this.shortletData.price)
+    
+    this.calculateNumberOfNights = (this.shortletData.price *  this.numberOfNights)
+    
+    
+    this.total = this.calculateNumberOfNights + 107 + 231;
   }
-  
-  // date14: Date;
 }
