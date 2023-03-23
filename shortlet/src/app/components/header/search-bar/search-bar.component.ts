e4ca0f0e3e093e38e50f1aff5d02c5dcea50c47a
Ladenika <ladenika@systemspecs.com.ng>
2023-03-23 12:01:13 +0100

    filter bar

--- shortlet/src/app/components/header/search-bar/search-bar.component.ts
+++ shortlet/src/app/components/header/search-bar/search-bar.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent {
+  // range = new FormGroup({
+  //   start: new FormControl<Date | null>(null),
+  //   end: new FormControl<Date | null>(null),
+  // });
 
+  selected: Date | null;
+  selected1: Date | null;
 }
