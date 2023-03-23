9d1dcfad387625eed889b5167d03b9ffc9b3ce41
Ladenika <ladenika@systemspecs.com.ng>
2023-03-23 14:08:05 +0100

    filter bar added

--- shortlet/src/app/components/header/filter-bar/filter-bar.component.ts
+++ shortlet/src/app/components/header/filter-bar/filter-bar.component.ts
@@ -3,8 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-filter-bar',
   templateUrl: './filter-bar.component.html',
-  styleUrls: ['./filter-bar.component.css']
+  styleUrls: ['./filter-bar.component.css'],
 })
 export class FilterBarComponent {
-
+  filterbar: { name: string; image: string }[] = [
+    {
+      name: 'APARTMENT',
+      image: '../../../../assets/property/apatment.jpg',
+    },
+    {
+      name: 'GUESTHOUSE',
+      image: '../../../../assets/property/guest-house.jpg',
+    },
+    {
+      name: 'BED_AND_BREAKFAST',
+      image: '../../../../assets/property/bed-breakfast.jpg',
+    },
+    {
+      name: 'TINY_HOME',
+      image: '../../../../assets/property/tiny-home.jpg',
+    },
+    {
+      name: 'CASTLE',
+      image: '../../../../assets/property/castles.jpg',
+    },
+    {
+      name: 'HOTEL',
+      image: '../../../../assets/property/hotel.jpg',
+    },
+    {
+      name: 'TENT',
+      image: '../../../../assets/property/tent.jpg',
+    },
+    {
+      name: 'TREEHOUSE',
+      image: '../../../../assets/property/tree-house.jpg',
+    },
+  ];
 }
