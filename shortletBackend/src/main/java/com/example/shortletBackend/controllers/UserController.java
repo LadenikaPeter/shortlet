69641a45328e184b1ba6942d1d4e5dd80d07a975
Bassey <inameti@systemspecs.com.ng>
2023-03-25 15:20:23 +0100

    when update user is called it return a UsersDTO with all the updated field and renamed all repositories with repo to Repository

--- shortletBackend/src/main/java/com/example/shortletBackend/controllers/UserController.java
+++ shortletBackend/src/main/java/com/example/shortletBackend/controllers/UserController.java
@@ -1,13 +1,14 @@
 package com.example.shortletBackend.controllers;
 
+import com.example.shortletBackend.dto.UsersDTO;
 import com.example.shortletBackend.entities.Apartments;
 import com.example.shortletBackend.entities.Reservation;
 import com.example.shortletBackend.entities.Users;
 import com.example.shortletBackend.enums.ReservationState;
 import com.example.shortletBackend.enums.Role;
-import com.example.shortletBackend.repositories.ApartmentRepo;
-import com.example.shortletBackend.repositories.ReservationRepo;
-import com.example.shortletBackend.repositories.UserRepo;
+import com.example.shortletBackend.repositories.ApartmentRepository;
+import com.example.shortletBackend.repositories.ReservationRepository;
+import com.example.shortletBackend.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
@@ -20,23 +21,23 @@ import java.util.Optional;
 @CrossOrigin
 @AllArgsConstructor
 public class UserController {
-    private final UserRepo userRepo;
-    private final ApartmentRepo apartmentRepo;
-    private final ReservationRepo reservationRepo;
+    private final UserRepository userRepository;
+    private final ApartmentRepository apartmentRepo;
+    private final ReservationRepository reservationRepo;
     private final ModelMapper mapper;
 
 
     @GetMapping("/")
     public ResponseEntity getUser(@RequestHeader("user_email")String email){
-        return ResponseEntity.of(userRepo.findUsersByEmail(email));
+        return ResponseEntity.of(userRepository.findUsersByEmail(email));
     }
 
     @PostMapping("/signup")
     public ResponseEntity signUp(@RequestBody Users users){
-        Optional<Users> oldUser = userRepo.findUsersByEmail(users.getEmail());
+        Optional<Users> oldUser = userRepository.findUsersByEmail(users.getEmail());
         if (!oldUser.isPresent()) {//if the user does not exist it creates a new user
             users.setRole(Role.USER);
-            userRepo.save(users);
+            userRepository.save(users);
             return ResponseEntity.ok(users);
         }else {// if it does exist, the user is then returned
             return ResponseEntity.ok(oldUser);
@@ -47,7 +48,7 @@ public class UserController {
     @PutMapping("/update_user/")
     public ResponseEntity updateUser(@RequestHeader("user_email") String email,
          @RequestBody Users users){
-        Optional<Users> oldInfo= userRepo.findUsersByEmail(email);
+        Optional<Users> oldInfo= userRepository.findUsersByEmail(email);
         if (oldInfo.isPresent()){
 
             if (users.getPhoneNo() != null) {
@@ -57,8 +58,8 @@ public class UserController {
                 oldInfo.get().setName(users.getName());
             }
 
-            userRepo.save(oldInfo.get());
-            return ResponseEntity.ok("Successfully updated profile");
+            userRepository.save(oldInfo.get());
+            return ResponseEntity.ok(mapper.map(oldInfo.get(), UsersDTO.class));
         }
         else {
             return (ResponseEntity) ResponseEntity.notFound();
@@ -69,7 +70,7 @@ public class UserController {
     @PutMapping("/addReservation/")
     public ResponseEntity addReservation(@RequestBody Reservation reservation,
          @RequestParam("user_email")String email,@RequestParam("apartment_id") long home_id){
-        Optional<Users> user = userRepo.findUsersByEmail(email);
+        Optional<Users> user = userRepository.findUsersByEmail(email);
         Optional<Apartments> apartments= apartmentRepo.findById(home_id);
 
         if(user.isPresent()){
@@ -83,7 +84,7 @@ public class UserController {
 
                 apartmentRepo.save(apartments.get());
                 reservationRepo.save(reservation);
-                userRepo.save(user.get());
+                userRepository.save(user.get());
                 return ResponseEntity.ok("Successfully reserved");
             }else {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The house can not be found");
@@ -100,7 +101,7 @@ public class UserController {
 //        for (Reservation reservation: ){
 //            reservationDTOS.add(mapper.map(reservation, ReservationDTO.class));
 //        }
-        return ResponseEntity.ok(reservationRepo.findAllByUsers(userRepo.findUsersByEmail(email).get()));
+        return ResponseEntity.ok(reservationRepo.findAllByUsers(userRepository.findUsersByEmail(email).get()));
     }
 
 
