5d2a332e9db4f64f5b94ac8f23be5214c1d976cb
Ladenika <ladenika@systemspecs.com.ng>
2023-03-31 21:05:16 +0100

    responsivenessa added

--- shortlet/src/app/components/header/filter-bar/filter-bar.component.ts
+++ shortlet/src/app/components/header/filter-bar/filter-bar.component.ts
@@ -10,38 +10,47 @@ export class FilterBarComponent {
   constructor(private dataS: DataStorageService) {}
   toggleActiveApartment: string;
 
-  filterbar: { name: string; image: string }[] = [
+  filterbar: { name: string; image: string; value: string }[] = [
     {
-      name: 'APARTMENT',
+      name: 'Apartment',
       image: '../../../../assets/property/apatment.jpg',
+      value: 'APARTMENT',
     },
     {
-      name: 'GUESTHOUSE',
+      name: 'Guest House',
       image: '../../../../assets/property/guest-house.jpg',
+      value: 'GUESTHOUSE',
     },
+
     {
-      name: 'BED_AND_BREAKFAST',
-      image: '../../../../assets/property/bed-breakfast.jpg',
-    },
-    {
-      name: 'TINY_HOME',
+      name: 'Tiny Home',
       image: '../../../../assets/property/tiny-home.jpg',
+      value: 'TINY_HOME',
     },
     {
-      name: 'CASTLE',
+      name: 'Castle',
       image: '../../../../assets/property/castles.jpg',
+      value: 'CASTLE',
     },
     {
-      name: 'HOTEL',
+      name: 'Hotel',
       image: '../../../../assets/property/hotel.jpg',
+      value: 'HOTEL',
     },
     {
-      name: 'TENT',
+      name: 'Bed & Breakfast',
+      image: '../../../../assets/property/bed-breakfast.jpg',
+      value: 'BED_AND_BREAKFAST',
+    },
+    {
+      name: 'Tent',
       image: '../../../../assets/property/tent.jpg',
+      value: 'TENT',
     },
     {
-      name: 'TREEHOUSE',
+      name: 'Tree house',
       image: '../../../../assets/property/tree-house.jpg',
+      value: 'TREEHOUSE',
     },
   ];
 
