88aa5700d27fedf6e7e7098052703d23cb983da7
Ladenika <ladenika@systemspecs.com.ng>
2023-04-05 09:02:56 +0100

    admin page to return all users

--- shortlet/src/app/components/admin/admin.component.ts
+++ shortlet/src/app/components/admin/admin.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable, Observer } from 'rxjs';
+import { DataStorageService } from 'src/app/services/data-storage.service';
 
 export interface ExampleTab {
   label: string;
   content: string;
+}
+
+export interface UserData {
+  id: number;
+  name: string;
 }
 
 @Component({
@@ -11,18 +17,33 @@ export interface ExampleTab {
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css'],
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+  users: any;
   asyncTabs: Observable<ExampleTab[]>;
 
-  constructor() {
-    this.asyncTabs = new Observable((observer: Observer<ExampleTab[]>) => {
-      setTimeout(() => {
-        observer.next([
-          { label: 'First', content: 'Content 1' },
-          { label: 'Second', content: 'Content 2' },
-          { label: 'Third', content: 'Content 3' },
-        ]);
-      }, 1000);
-    });
+  displayedColumns: string[] = [''];
+  constructor(private dataS: DataStorageService) {
+    //   this.asyncTabs = new Observable((observer: Observer<ExampleTab[]>) => {
+    //     setTimeout(() => {
+    //       observer.next([
+    //         { label: 'First', content: 'Content 1' },
+    //         { label: 'Second', content: 'Content 2' },
+    //         { label: 'Third', content: 'Content 3' },
+    //       ]);
+    //     }, 1000);
+    //   });
   }
+
+  ngOnInit(): void {
+    this.dataS.getAllUsers();
+  }
+
+  // applyFilter(event: Event) {
+  //   const filterValue = (event.target as HTMLInputElement).value;
+  //   this.dataSource.filter = filterValue.trim().toLowerCase();
+
+  //   if (this.dataSource.paginator) {
+  //     this.dataSource.paginator.firstPage();
+  //   }
+  // }
 }
